363cd02af42de5c6b094b43a1021c9aec66a8995
swimtrunk <swimtrunkgames@gmail.com>
2021-11-18 17:58:16 -0600

    Power Up Duration & Sprites

--- Assets/Scripts/PowerUp.cs
+++ Assets/Scripts/PowerUp.cs
@@ -6,6 +6,8 @@ public class PowerUp : MonoBehaviour
 {
     //Circle rotation variables
     float timeCounter = 0f;
+    int timeRounded;
+    public int durationTime;
 
     float x = 0f;
     float y = 0f;
@@ -15,17 +17,25 @@ public class PowerUp : MonoBehaviour
 
     //Power up variables
     public Sprite[] powerUpSprites;
+    Sprite spriteToUse;
 
     public GameObject powerUp;
     PowerUpGrab powerUpGrabScript;
 
     void Start()
     {
+        spriteToUse = powerUpSprites[Random.Range(0, powerUpSprites.Length)];
+        powerUp.GetComponent<SpriteRenderer>().sprite = spriteToUse;
         powerUpGrabScript = powerUp.GetComponent<PowerUpGrab>();
     }
 
     void Update()
     {
+        if (timeRounded % durationTime == 0 && timeRounded > 0)
+        {
+            powerUp.SetActive(true);
+        }
+
         if (powerUpGrabScript.powerGrabbed)
         {
             //Give Power Up
@@ -34,6 +44,7 @@ public class PowerUp : MonoBehaviour
 
         //Circle rotation handling
         timeCounter += Time.deltaTime;
+        timeRounded = Mathf.RoundToInt(timeCounter);
         x = Mathf.Cos(timeCounter) * radius;
         y = Mathf.Sin(timeCounter) * radius;
 
